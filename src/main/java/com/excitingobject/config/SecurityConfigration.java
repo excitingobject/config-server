963fe685b91fd36f9cb8a48029bc950cf62f7f60
yoonseok-eo <eo.yoonseok.0918@gmail.com>
2023-10-11 16:00:04 +0900

    ...

---
+++ src/main/java/com/excitingobject/config/SecurityConfigration.java
@@ -0,0 +1,28 @@
+package com.excitingobject.config;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@ConditionalOnProperty(name = "excitingobject.security.enable", havingValue = "true")
+public class SecurityConfigration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.GET).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .rememberMe(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
